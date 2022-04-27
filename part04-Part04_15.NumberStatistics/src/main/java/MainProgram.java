4df9e54b8997a183ad072b8bb9b7d8fbad4c1ad6
Skepsis101 <kevinpatriciosp@gmail.com>
2022-04-27 16:20:07 -0300

    27/04/22

--- part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
+++ part04-Part04_15.NumberStatistics/src/main/java/MainProgram.java
@@ -4,13 +4,37 @@ import java.util.Scanner;
 public class MainProgram {
 
     public static void main(String[] args) {
+        System.out.println("Enter numbers:");
         Scanner scanner = new Scanner(System.in);
-        // you can write test code here
-        // however, remove all unnecessary code when doing the final parts of the exercise
 
-        // In order for the tests to work, the objects must be created in the
-        // correct order in the main program. First the object that tracks the total
-        // sum, secondly the object that tracks the sum of even numbers, 
-        // and lastly the one that tracks the sum of odd numbers!
+        Statistics statistics = new Statistics();
+        Statistics statistics2 = new Statistics();
+        Statistics statistics3 = new Statistics();
+
+        int input = 0;
+
+        // some todos os numeros
+        while (true) {
+            input = Integer.valueOf(scanner.nextInt());
+            if (input == -1) { // cheque se é -1 e saia do loop
+                break;
+            }
+            statistics.addNumber(input); // se for diferente de -1, chame o método para adicionar fora do condicional
+
+            // some os pares
+            if (input % 2 == 0) {
+                statistics2.addNumber(input);
+            } else {
+                // some os ímpares
+                statistics3.addNumber(input);
+            }
+        }
+
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
+        System.out.println("Sum of even numbers: " + statistics2.sum());
+        System.out.println("Sum of odd numbers: " + statistics3.sum());
+
     }
 }
--- part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
+++ part04-Part04_15.NumberStatistics/src/main/java/Statistics.java
@@ -1,3 +1,36 @@
 
 public class Statistics {
+    private int count;
+    private int sum;
+
+    public Statistics() {
+        this.count = 0;
+        this.sum = 0;
+    }
+
+    // incremente a soma e a quantidade
+    public void addNumber(int number) {
+        this.sum = this.sum + number;
+        this.count++;
+    }
+
+    // mostre a quantidade
+    public int getCount() {
+        return this.count;
+    }
+
+    // mostra a soma
+    public int sum() {
+        return this.sum;
+    }
+
+    // calcule a média
+    public double average() {
+        // cheque para não dividr por zero
+        if (this.count == 0) {
+            return 0.0;
+        }
+        return (double) this.sum / (double) this.count;
+    }
+
 }
